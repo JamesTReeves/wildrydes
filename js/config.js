1e61763a772a77a7750c4fe89b1045591c67c86b
JamesTReeves <jreeves@inline.group>
2020-07-28 10:49:36 -0500

    website config

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-2_M3PYlFKFN', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '7d65d7iek6top3j1eps47on3l5', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
